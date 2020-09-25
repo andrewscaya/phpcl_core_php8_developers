84c82df8d7faf4f9c89c115bf74b204694e19778
Andrew Caya <andrewscaya@yahoo.ca>
2020-09-25 12:45:10 -0400

    2020-09-25 AC: Adds a lab solution from Richard/Andrew.

--- labs/ext_gotchas/get_info.php
+++ labs/ext_gotchas/get_info.php
@@ -1,25 +1,27 @@
 <?php
+// FROM Richard and Andrew
 // get_info.php
 // called by /examples/core_lab_ext.php
 
-function getInfo($url, $search, $encode)
+function getInfo(...$info)
 {
-	$ch = curl_init($url);
-	if (!is_resource($ch)) return FALSE;
-	
-	$result = [];
-	curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-	$codes = curl_exec($ch);
-	if ($codes) {
-		$codes = ($codes) ? json_decode($codes) : [];
-		if ($codes) {
-			foreach ($codes->data as $item) {
-				$found = mb_strrpos($item->city, $search, $encode);
-				if ($found) $result[] = $item;
-			}
-		}
-	}
-	return $result;
+    $ch = curl_init($info[0]);
+
+    if (!is_resource($ch) && !$ch instanceof CurlHandle) return FALSE;
+
+        $result = [];
+        curl_setopt($ch, CURLOPT_URL, $info[0]);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HEADER, false);
+        $codes = curl_exec($ch);
+        if ($codes) {
+            $codes = ($codes) ? json_decode($codes) : [];
+            if ($codes) {
+                foreach ($codes->data as $item) {
+                    $found = mb_strrpos($item->city, $info[1], 0, $info[2]);
+                    if ($found) $result[] = $item;
+                }
+            }
+        }
+        return $result;
 }
